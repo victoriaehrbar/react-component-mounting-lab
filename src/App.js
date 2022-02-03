ab1ee51d7fda77786c0cee8d42a9746876191935
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-02-03 17:34:01 -0600

    commit tree 316101f16433b69a0ec40747dc022b41179f5380

--- src/App.js
+++ src/App.js
@@ -12,16 +12,10 @@ class App extends Component {
 
   //Your code here:
 
-
-
-
-
-
-
-
-
-  // No need to modify anything in render or the class methods below
-  // Unless, of course, you're curious about how it all works
+  componentDidMount() {
+    this.handleAddTimer()
+  }
+  
   render() {
 
     return (
@@ -37,19 +31,17 @@ class App extends Component {
     );
   }
 
-  // returns array of components written in JSX, mapped from this.state.timerIDs
   renderTimers = () => this.state.timerIDs.map(id => {
     return <Timer key={id} id={id} removeTimer={this.removeTimer} />
   })
 
-  // adds a random number for timer ID
   handleAddTimer = () => {
     this.setState(prevState => ({
       timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
     }))
   }
 
-  // removeTimer updates state, removing any timer that matches the provided author
+  
   removeTimer = id => {
     this.setState(prevState => ({
       timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
